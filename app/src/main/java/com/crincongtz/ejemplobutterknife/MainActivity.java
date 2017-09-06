f95c7f8d3ea49c885cdb6397be42e9aa28982023
Christian Rincón <rigc88@hotmail.com>
2017-09-06 18:01:10 -0500

    Button with click action

--- app/src/main/java/com/crincongtz/ejemplobutterknife/MainActivity.java
+++ app/src/main/java/com/crincongtz/ejemplobutterknife/MainActivity.java
@@ -2,15 +2,26 @@ package com.crincongtz.ejemplobutterknife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textViewA;
+
     @BindView(R.id.textB) TextView textViewB;
+    @BindView(R.id.boton) Button boton;
+
+    @OnClick(R.id.boton)
+    public void clicked(){
+        Toast.makeText(this, "Click with ButterKnife", Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         textViewA.setText("Texto C");
 
         textViewB.setText("Texto D");
+
+
 
     }
 }
