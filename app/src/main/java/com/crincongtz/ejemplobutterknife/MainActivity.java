2b37f54ec0f47185c957eee43319973b0bb86534
Christian Rincón <rigc88@hotmail.com>
2017-09-06 17:17:22 -0500

    Textview basic example

--- app/src/main/java/com/crincongtz/ejemplobutterknife/MainActivity.java
+++ app/src/main/java/com/crincongtz/ejemplobutterknife/MainActivity.java
@@ -2,12 +2,26 @@ package com.crincongtz.ejemplobutterknife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewA;
+    @BindView(R.id.textB) TextView textViewB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        textViewA = (TextView) findViewById(R.id.textA);
+        textViewA.setText("Texto C");
+
+        textViewB.setText("Texto D");
+
     }
 }
